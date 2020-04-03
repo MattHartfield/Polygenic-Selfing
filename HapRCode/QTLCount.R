cbf4e3d017a8c59e1f1ce53ab4f23d8ecc4007a7
Matthew Hartfield <m.hartfield@ed.ac.uk>
2020-04-03 16:49:50 +0100

    Corrected QTL print script when no QTLs exist

--- HapRCode/QTLCount.R
+++ HapRCode/QTLCount.R
@@ -18,6 +18,10 @@ for(i in 1:length(pt))
 	pdf(file=paste0('/Users/hartfield/Documents/Polygenic Selection Selfing/SLiM Scripts/ServerPlots/haps/HS_',pt[i],'_s',s,'_h',h,'_self',S,'_nt',N,'_newo',z1,'_msd',msd,'.count.pdf'),width=8,height=18)
 	par(mfrow=c(2,1))
 	barplot(table(dat$V1),ylab="Count")
-	hist(dat$V2,col="gray70",xlab="Mean QTL effect per individual",main="")
+	if(is.na(dat$V2[1])!=T){
+		hist(dat$V2,col="gray70",xlab="Mean QTL effect per individual",main="")	
+	}else{
+		hist(dat$V1,border="white",xlab="Mean QTL effect per individual",main="")	
+	}
 	dev.off()
 }
