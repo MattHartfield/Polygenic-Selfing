fb3c45ac28627fbc3250b31a2d67e36623cd0978
Matthew Hartfield <m.hartfield@ed.ac.uk>
2020-09-14 14:40:37 +0100

    Updated new haplotype plot script; added measure of genetic variance to simulation code

--- ServerScripts/Hap_Plot_QTL.R
+++ ServerScripts/Hap_Plot_QTL.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(RColorBrewer)
 
-filenames <- c('beforeshift','20gens','150gens')
+filenames <- c('0gens','20gens','150gens')
 
 for(k in filenames){
 
@@ -13,34 +13,39 @@ for(k in filenames){
 	fc <- c()
 	for(a in 0:49){
 		if(a!=49){
-			dat <- separate(dat,paste0("i",a),c(paste0("A",a),paste0("B",a),paste0("C",a),paste0("D",a),paste0("E",a)),sep="|") %>% select(c(paste0("B",0:a),paste0("D",0:a),paste0("i",(a+1):49)))
+			dat <- separate(dat,paste0("i",a),c(paste0("X",a),paste0("A",a),paste0("Y",a),paste0("B",a),paste0("Z",a)),sep="|") %>% select(c(paste0("A",0:a),paste0("B",0:a),paste0("i",(a+1):49)))
 		}else{
-			dat <- separate(dat,paste0("i",a),c(paste0("A",a),paste0("B",a),paste0("C",a),paste0("D",a),paste0("E",a)),sep="|") %>% select(c(paste0("B",0:a),paste0("D",0:a)))
+			dat <- separate(dat,paste0("i",a),c(paste0("X",a),paste0("A",a),paste0("Y",a),paste0("B",a),paste0("Z",a)),sep="|") %>% select(c(paste0("A",0:a),paste0("B",0:a)))
 		}
 		fc <- c(fc,a,a+50)
 	}
 	dat <- dat[,fc+1]
 	
-	# Plotting heatmap of just polymorphisms. NOTE USE OF 'SCALE=NONE', IMPORTANT!!!!
+	# Plotting heatmap of just polymorphisms without QTL colour coding. NOTE USE OF 'SCALE=NONE', IMPORTANT!!!!
 	# heatmap(t(data.matrix(dat)),Colv=NA,Rowv=NA,col=c("white","black"),scale="none")
 	
 	# Loading QTLs and assigning colour based on direction, mean strength
 	
-	hqc <- brewer.pal(9,"Reds")[5:8]
-	lqc <- brewer.pal(9,"Blues")[6:9]
+	hqc <- rev(brewer.pal(11,"RdBu")[1:5])
+	lqc <- brewer.pal(11,"RdBu")[7:11]
 	
 	QTLd <- read_delim(paste0("VCFout_test_",k,".info"),delim=" ")
 	QTLd <- QTLd[order(QTLd$POS),]
-	mq <-  max(QTLd[QTLd$MeanQTL>0,2])
-	minq <- min(QTLd[QTLd$MeanQTL<0,2])
-	QTLd <- QTLd %>% mutate(QTLs=ifelse(MeanQTL>=0, ceiling(MeanQTL/mq*4), ceiling(MeanQTL/minq*4) )) %>% mutate(QTLcol=ifelse(MeanQTL>=0, hqc[QTLs], lqc[QTLs] ))
+#	mq <-  max(QTLd[QTLd$MeanQTL>0,2])
+#	minq <- min(QTLd[QTLd$MeanQTL<0,2])
+	QTLd <- QTLd %>% mutate(QTLs=ifelse(MeanQTL>=0, ceiling(MeanQTL*8), ceiling(MeanQTL*(-8)) ))
+	QTLd[QTLd$QTLs>5,3] <- 5
+	QTLd <- QTLd %>% mutate(QTLcol=ifelse(MeanQTL>=0, hqc[QTLs], lqc[QTLs] ))
 	
 	plotc <- c("white","black")
 	QTLc <- 2
+	Qidx <- c()
 	for(b in 1:dim(QTLd)[1]){
 		if(length(which(row.names(dat)%in%QTLd[b,1]))!=0){
-			dat[which(row.names(dat)%in%QTLd[b,1]),dat[which(row.names(dat)%in%QTLd[b,1]),]==1] <- QTLc
+			Qidx2 <- which(row.names(dat)%in%QTLd[b,1])
+			dat[Qidx2,dat[Qidx2,]==1] <- QTLc
 			plotc <- c(plotc,QTLd[b,4])
+			Qidx <- c(Qidx,Qidx2)
 			QTLc <- QTLc + 1
 		}
 	}
@@ -48,7 +53,7 @@ for(k in filenames){
 	
 	# Creating sub matrix with stripped down entries; QTLs and other sites, no more than 100
 	if(dim(dat)[1]>100){
-		dat2 <- dat[sort(c( sample(which(rowSums(dat>=2)==0),100-length(which(rowSums(dat>=2)>0))), which(rowSums(dat>=2)>0) )),]
+		dat2 <- dat[sort(c( sample( which(!(c(1:dim(dat)[1])%in%Qidx)) ,100-length(Qidx)), Qidx )),]
 	}
 	
 	pdf(paste0("Hap_Plot_",k,".pdf"),width=12,height=12)
